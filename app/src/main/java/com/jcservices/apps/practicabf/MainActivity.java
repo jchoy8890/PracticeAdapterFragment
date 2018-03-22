9ea33b5a9a45508ad54dc52586dc0d8ca5377467
jchoy8890 <jchoy8890@gmail.com>
2018-03-22 12:59:22 -0500

    Changes for code load fragment

--- app/src/main/java/com/jcservices/apps/practicabf/MainActivity.java
+++ app/src/main/java/com/jcservices/apps/practicabf/MainActivity.java
@@ -1,12 +1,14 @@
 package com.jcservices.apps.practicabf;
 
 import android.app.FragmentManager;
-import android.content.Context;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.jcservices.apps.practicabf.bean.Product;
@@ -17,10 +19,10 @@ public class MainActivity extends AppCompatActivity implements OnProductSelected
     FragmentManager fManager;
 
     ProductSelectedFragment productSelectedFragment;
+    ListProductFragment listProductFragment;
 
     FloatingActionButton btnAdd;
 
-    private Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +33,35 @@ public class MainActivity extends AppCompatActivity implements OnProductSelected
     }
 
     private void loadUI() {
-        ctx = this;
-        productSelectedFragment = (ProductSelectedFragment) fManager.findFragmentById(R.id.fragmentDetailsProducts);
+        fManager.beginTransaction();
+        FragmentTransaction fmTransaction = fManager.beginTransaction();
+        FrameLayout frameDetailProduct = findViewById(R.id.frameDetailProduct);
+        listProductFragment = ListProductFragment.newInstance(null, null);
+        productSelectedFragment = ProductSelectedFragment.newInstance(null, null);
+        fmTransaction.replace(R.id.frameListProduct, listProductFragment);
+        if (frameDetailProduct != null)
+            fmTransaction.replace(R.id.frameDetailProduct, productSelectedFragment);
+        fmTransaction.commit();
         btnAdd = (FloatingActionButton) findViewById(R.id.btnAddProduct);
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ctx,"Add", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Add", Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Toast.makeText(getApplicationContext(), String.valueOf((FloatingActionButton) findViewById(R.id.btnAddProduct) != null), Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
     public void onClickProduct(Product product) {
-        if (productSelectedFragment != null) {
+        //Toast.makeText(getApplicationContext(), "Prueba", Toast.LENGTH_SHORT).show();
+        if (productSelectedFragment != null && productSelectedFragment.isVisible()) {
+            Log.i("JCHOY", "Fragment en pantalla.");
             sendDataToFragment(product);
         } else {
+            Log.i("JCHOY", "Yendo a Activity");
             startActivityDetailProduct(product);
         }
 
--- app/src/main/java/com/jcservices/apps/practicabf/ProductDetailActivity.java
+++ app/src/main/java/com/jcservices/apps/practicabf/ProductDetailActivity.java
@@ -1,14 +1,19 @@
 package com.jcservices.apps.practicabf;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
+    FragmentManager fManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+
     }
 
 
