9d6d5a423b95ca8f8b8058390ca33cd40e387e7a
jchoy8890 <jchoy8890@gmail.com>
2018-03-22 22:34:26 -0500

    Upload changes

--- app/src/main/java/com/jcservices/apps/practicabf/ProductDetailActivity.java
+++ app/src/main/java/com/jcservices/apps/practicabf/ProductDetailActivity.java
@@ -1,8 +1,10 @@
 package com.jcservices.apps.practicabf;
 
 import android.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jcservices.apps.practicabf.bean.Product;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
@@ -13,9 +15,9 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
-
+        Bundle extras = getIntent().getExtras();
+        Product p = extras.getParcelable("Product");
     }
-
 
 
 }
